c7ee32536a67d5a63bc583407e701dfae9d71b89
Alisonjs0 <alisonserafim.tec@gmail.com>
2025-11-28 13:48:47 -0300

    fix(security): Validação de entrada adicionado

--- back/src/main/java/com/example/SGE/DTO/RegisterRequestDTO.java
+++ back/src/main/java/com/example/SGE/DTO/RegisterRequestDTO.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 
 public record RegisterRequestDTO(
     @NotBlank(message = "O nome é obrigatorio")
+    @jakarta.validation.constraints.Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
     String nome,
 
     @NotBlank(message = "O email é obrigatorio")
@@ -15,11 +16,14 @@ public record RegisterRequestDTO(
     String email,
 
     @NotBlank(message = "O cpf é obrigatorio")
+    @jakarta.validation.constraints.Pattern(regexp = "\\d{11}", message = "CPF deve ter 11 dígitos numéricos")
     String cpf,
 
     @NotBlank(message = "A senha é obrigatória")
+    @jakarta.validation.constraints.Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{6,}$", message = "Senha deve ter no mínimo 6 caracteres, incluindo maiúscula, minúscula, número e caractere especial")
     String senha,
 
+    @NotBlank(message = "O cargo é obrigatório")
     String cargo,
     
     @CreatedDate
